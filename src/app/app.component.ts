07c46a8094b73795afcc45dd59df0fe425408b76
Gustavo Clay <gustavo.lima@eigmercados.com.br>
2019-03-26 20:27:49 -0300

    adicao da biblioteca de ui, testes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuItem } from 'primeng/api';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'geduca-webapp';
-  items: MenuItem[];
 
-  ngOnInit(): void {
-    this.items = [
-      {
-        label: 'File',
-        items: [{
-          label: 'New',
-          icon: 'pi pi-fw pi-plus',
-          items: [
-            { label: 'Project' },
-            { label: 'Other' },
-          ]
-        },
-        { label: 'Open' },
-        { label: 'Quit' }
-        ]
-      },
-      {
-        label: 'Edit',
-        icon: 'pi pi-fw pi-pencil',
-        items: [
-          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
-          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
-        ]
-      }
-    ];
-  }
 }
