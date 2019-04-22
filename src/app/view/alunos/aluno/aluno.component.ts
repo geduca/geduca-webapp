7c45130982c2de613273eead31ae0e0c56fc0e7c
Gustavo Clay <gustavoclay@gmail.com>
2019-04-22 10:58:04 -0300

    Continuar implementação do cadastro de aluno

--- src/app/view/alunos/aluno/aluno.component.ts
+++ src/app/view/alunos/aluno/aluno.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Aluno } from 'src/app/model/Aluno';
+import { Pessoa } from 'src/app/model/Pessoa';
 import { AlunoService } from 'src/app/service/aluno.service';
+
+import { Endereco } from './../../../model/Endereco';
+
 
 
 @Component({
@@ -8,10 +14,46 @@ import { AlunoService } from 'src/app/service/aluno.service';
 })
 export class AlunoComponent implements OnInit {
 
-  constructor(private alunoService: AlunoService) {
+  aluno: Aluno;
+  pessoa: Pessoa;
+  alunoForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private alunoService: AlunoService) {
   }
 
   ngOnInit() {
+    this.alunoForm = this.formBuilder.group({
+      nome: [''], cpf: [''], dataNascimento: [''], sexo: [''], pai: [''], mae: [''], email: [''],
+      telefone: [''], celular: [''], cep: [''], logradouro: [''], numero: [''], complemento: [''],
+      bairro: [''], cidade: [''], estado: ['']
+    });
+  }
+
+  cadastrar() {
+    const p = new Pessoa();
+    const a = new Aluno();
+    const e = new Endereco();
+    p.nome = this.alunoForm.get('nome').value;
+    p.cpf = this.alunoForm.get('cpf').value;
+    p.dataNascimento = this.alunoForm.get('dataNascimento').value;
+    p.sexo = this.alunoForm.get('sexo').value;
+    p.pai = this.alunoForm.get('pai').value;
+    p.mae = this.alunoForm.get('mae').value;
+    p.email = this.alunoForm.get('email').value;
+    p.telefone = this.alunoForm.get('telefone').value;
+    p.celular = this.alunoForm.get('celular').value;
+    e.cep = this.alunoForm.get('cep').value;
+    e.logradouro = this.alunoForm.get('logradouro').value;
+    e.numero = this.alunoForm.get('numero').value;
+    e.complemento = this.alunoForm.get('complemento').value;
+    e.bairro = this.alunoForm.get('bairro').value;
+    e.cidade = this.alunoForm.get('cidade').value;
+    e.estado = this.alunoForm.get('estado').value;
+
+    p.endereco = e;
+    a.pessoa = p;
+
+    console.log(a);
   }
 
 }
