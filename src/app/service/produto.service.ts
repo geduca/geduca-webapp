151de3289b71c052fdcc42d58da7aee884297d43
Mateus Henrique <mateus_twister@hotmail.com>
2019-05-16 00:02:14 -0300

    Correçao

--- src/app/service/produto.service.ts
+++ src/app/service/produto.service.ts
@@ -1,4 +1,3 @@
-import { Fornecedor } from './../model/Fornecedor';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
