87802d9655e522514e44f3b00076296ff37f858c
Gustavo Clay <gustavoclay@gmail.com>
2019-05-22 03:23:12 -0300

    teste

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   apiUrl: 'https://geduca-api.herokuapp.com/geduca',
 
   // token oauth0
-  tokenWhitelistedDomains: [new RegExp('geduca-api.herokuapp.com')],
+  tokenWhitelistedDomains: [new RegExp('geduca-api.herokuapp.com'), 'geduca-api.herokuapp,com'],
   tokenBlacklistedRoutes: [new RegExp('\/oauth\/token')]
 };
